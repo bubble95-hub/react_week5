9cfeb6b5d6d14118adb8bbbec685c5d2f001eb09
bubble <hanahana1995@gmail.com>
2026-02-04 12:34:42 +0800

    set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // 開發中、產品路徑
-  base: process.env.NODE_ENV === "production" ? "/react_course/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/react_week5/" : "/",
   plugins: [react()],
 });
